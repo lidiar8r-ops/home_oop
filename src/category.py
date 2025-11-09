06e4bacecf03016ddb06122d38340b78980475db
Рыбина Лидия <lidiar@rambler.ru>
2025-11-09 19:29:34 +0500

    update category.pu and product.py

--- src/category.py
+++ src/category.py
@@ -1,3 +1,15 @@
+from src.product import Product
+
 
 class Category:
-    pass
+    # название
+    name: str
+    # описание
+    description: str
+    #список товаров категории
+    products : List[Product]
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
--- src/product.py
+++ src/product.py
@@ -1,3 +1,19 @@
 
 class Product:
-    pass
+    # название
+    name : str
+    # описание
+    description : str
+    # цена
+    price : float
+    # количество в наличии
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+
