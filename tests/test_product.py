6d08eae1125ba9711aa52814c8ee7324baaacfb2
Рыбина Лидия <lidiar@rambler.ru>
2025-11-17 22:33:01 +0500

    update  test_product

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 def test_product(protuct_one):
     assert protuct_one.name == "Samsung Galaxy S23 Ultra"
     assert protuct_one.description == "256GB, Серый цвет, 200MP камера"
@@ -5,3 +8,12 @@ def test_product(protuct_one):
     assert protuct_one.quantity == 5
 
 
+def test_product_new_product():
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    assert new_product.name == "Samsung Galaxy S23 Ultra"
+    assert new_product.description == "256GB, Серый цвет, 200MP камера"
+    assert new_product.price == 180000.0
+    assert new_product.quantity == 5
+    assert new_product.price == 180000.0
